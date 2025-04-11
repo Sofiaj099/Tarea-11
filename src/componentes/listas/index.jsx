76f007afea51cd8258fd395c9f4b93c28bc2f5b7
Juliana099 <146016662+Sofiaj099@users.noreply.github.com>
2025-04-11 18:21:08 +0000

    hola

--- src/componentes/listas/index.jsx
+++ src/componentes/listas/index.jsx
@@ -1,33 +1,29 @@
-import { useState, useEffect } from 'react'
-
-import './style.css'
+import { useState, useEffect } from 'react';
+import './style.css';
 
 function Listas() {
-
   const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=1025")
-      .then(response => response.json())
-      .then(responseData => setData(responseData.results))
-      .catch(error => console.error("Error:", error));
-  }, []); 
 
+  useEffect(() => {
+    fetch("https://universities.hipolabs.com/search?country=Argentina")
+      .then(response => response.json())
+      .then(responseData => setData(responseData))
+      .catch(error => console.error("Error:", error));
+  }, []);
 
   return (
-
     <section className='c-lista'>
-    {data.map((pokemon, index) => (
-      <div className='c-lista-pokemon'
-      key={index}>
-        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.url.split("/")[6]}.png`} 
-              alt={`Pokémon ${pokemon.name}`} width='auto' height='60' loading='lazy'
-            />
-        <p>{pokemon.name}</p>
-      </div>
-    ))}
-  </section>
-);
-
+      {data.map((university, index) => (
+        <div className='c-lista-pokemon' key={index}>
+          <p><strong>{university.name}</strong></p>
+          <p>{university.country}</p>
+          <a href={university.web_pages[0]} target='_blank' rel='noopener noreferrer'>
+            Visitar sitio web
+          </a>
+        </div>
+      ))}
+    </section>
+  );
 }
 
-export default Listas
+export default Listas;
