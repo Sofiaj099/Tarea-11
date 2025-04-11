989ad8387252e32824383783c82b4b8f76c390ff
Juliana099 <146016662+Sofiaj099@users.noreply.github.com>
2025-04-11 05:54:29 +0000

    Falta la API

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState} from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+
+import './App.css'
+import Listas from './componentes/listas'
+import Favoritos from './componentes/favoritos'
+import Aleatorio from './componentes/aleatorios'
+import Capturado from './componentes/capturados'  
+import Usuarios from './componentes/usuarios'
+import Pokemon from './componentes/pokemon'
+import Aleatorios from './componentes/aleatorios';
+import Capturados from './componentes/capturados';
+import Menu from './componentes/Menu';
+
+function App() {
+
+
+  return (
+    <Router>
+      <Menu/>
+      <Routes>
+      <Route path= "/" element={<Listas/>}/>
+      <Route path= "/Favoritos" element={<Favoritos/>}/>
+      <Route path= "/Aleatorios" element={<Aleatorios/>}/>
+      <Route path= "/Capturados" element={<Capturados/>}/>
+      <Route path= "/Usuarios" element={<Usuarios/>}/>
+      <Route path= "/Pokemon" element={<Pokemon/>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/componentes/capturados/index.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './style.css'
+
+function Capturados() {
+
+
+  return (
+    <>
+    <h1>Capturado</h1>
+
+    </>
+  )
+}
+
+export default Capturados
